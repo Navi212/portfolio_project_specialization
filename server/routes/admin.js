cb0fa950a359c59a027830db6869338a286bad36
Stanhope Arthur Chinonso <stanhope.carthur@gmail.com>
2024-03-03 16:03:55 +0100

    Title: Add Register form to HTML template
    Description:
    - Added a Register form to the HTML template.
    - Included input fields for username and password.
    - Set the form action to "/register" to handle form submission.
    
    Title: Import User model in router file
    
    Description:
    - Added import statement for the User model in the router file to use it for user registration.
    
    Title: Add User registration form to HTML template
    
    Description:
    - Added a registration form for users in the HTML template.
    - Included input fields for username and password.
    - Set the form action to "/register" to handle form submission.
    
    Title: Add POST route to check admin login
    
    Description:
    - Created a POST route '/admin' in the router file to handle admin login.
    - Retrieved username and password from the request body using destructuring.
    - Logged the request body for debugging purposes.
    - Redirected to the '/admin' page after login attempt.
    - Implemented error handling using try-catch block to log errors.

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 // const POst = require("../models/Post");
 const Post = require("../models/Post");
+const User = require("../models/User");
 
 const adminLayout = "../views/layouts/admin";
 
@@ -11,11 +12,6 @@ const adminLayout = "../views/layouts/admin";
  */
 
 router.get('/admin', async (req, res) => {
-  const locals = {
-    title: "Admin",
-    description: "Simple Blog created with NodeJs, Express & MongoDb."
-  }
-
   try {
     const locals = {
       title: "Admin",
@@ -24,6 +20,27 @@ router.get('/admin', async (req, res) => {
 
     //const data = await Post.find();
     res.render('admin/index', { locals, layout: adminLayout });
+  } catch (error) {
+    console.log(error);
+  }
+
+});
+
+
+/**
+ * POST /
+ * Admin - Check Login 
+ */
+
+router.post('/admin', async (req, res) => {
+  try {
+    const{ username, password } = req.body;
+    console.log(req.body);
+
+    //const data = await Post.find();
+    res.redirect('/admin');
+
+    
   } catch (error) {
     console.log(error);
   }
